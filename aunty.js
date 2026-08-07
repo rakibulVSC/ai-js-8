355e8a64e5fd64b8c22594b207465fa4a05636d3
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-07 22:29:32 +0600

    add if conditional

--- aunty.js
+++ aunty.js
@@ -1,1 +1,11 @@
-console.log('aunty')
+/**greater than or equal **/
+/* conditional 
+ */
+
+const rickshawFare=50;
+if(rickshawFare<70){
+    console.log("mama hod tule den")
+}
+else{
+    console.log("Walking is also exercise")
+}
