ced622e372258965d8cfa1bc4e8383936d9d43fe
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-08 09:31:11 +0600

    grading system

---
+++ grade.js
@@ -0,0 +1,22 @@
+const marks=45;
+if(marks>80){
+    console.log('A+')
+}
+else if(marks>70){
+    console.log('A')
+}
+else if(marks>60){
+    console.log('B+')
+}
+else if(marks>50){
+    console.log("B")
+}
+else if (marks>40){
+    console.log("C+")
+}
+else if(marks>33){
+    console.log("c")
+}
+else{
+    console.log("Fail")
+}
